d2b7800aeb1d834d4b747c0a43072fcb41e0fb57
sudhanshu3430 <127764778+sudhanshu3430@users.noreply.github.com>
2023-12-19 20:03:20 +0530

    changes

--- models/CustomerModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const customerModelSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true }
-});
-
-module.exports = mongoose.model("customer", customerModelSchema, "customer");
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,9 +1,35 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+import mongoose from "mongoose";
 
-var userSchema = new Schema({
-  userID: { type: String, required: true },
-  password: { type: String, required: true }
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("user", userSchema, "user");
+export default mongoose.model("users", userSchema);
